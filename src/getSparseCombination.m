b65690b7f11b6ca9ac12900d9d8f2d6a99255e5f
a <a>
2014-12-03 11:18:40 +0530

    getSparseCombination can now handle multiple columns simultaneously

--- src/getSparseCombination.m
+++ src/getSparseCombination.m
@@ -1,30 +1,36 @@
 % Gets a sparse representation of dat using elements of dict. 
 % Orthogonal matching algorithm
 % Inputs:   dict: atoms in columns
-%           dat: one column
+%           dat: k data columns
 %           T: max number of non-zero in sparseX
 
 function [sparseX] = getSparseCombination(dict, dat, T)
-   res = dat;
+   
+   res = dat; % k columns, residue of each data
    dictdim = size(dict);
    indices = [];
-   sparseX = zeros(dictdim(2),1 );
+   sparseX = zeros(dictdim(2),size(dat, 2));
    for i=1:T
        % Get inner products in a row
-       innerpdt = sum(dict .* repmat(res,1,dictdim(2)), 1);
+       innerpdt = dict' * dat; % inner pdt of one data is in a column. 
        % Get index of max inner product
-       [C, I] = max(abs(innerpdt));
+       [C, I] = max(abs(innerpdt)); % max indices for each data, all in one row
        % Set coefficient in return vector
-       sparseX(I) = innerpdt(I);
+       sparseX(sub2ind(size(innerpdt), I, 1:length(I))) = ...
+           innerpdt(sub2ind(size(innerpdt), I, 1:length(I)));
+       %sparseX(I) = innerpdt(I); %%%%%% LEFT HERE
        % Add to set of indices
-       indices = [indices, I];
-       % Orthogonalize the past chosen vector set
-       orth = orthogonalize(dict(:, indices));
-       % Get projection components
-       components = sum(orth .* repmat(dat, 1, size(orth, 2)), 1);
-       % Get projection
-       proj = sum( orth .* repmat(components, size(orth, 1), 1), 2);
-       % Get component outside subspace of chosen vectors
-       res = dat - proj;
+       indices = [indices; I];
+       for j=1:size(dat, 2)
+           % Orthogonalize the past chosen vector set
+           orth = orthogonalize(dict(:, indices(:,j)'));
+           % Get projection components
+           components = sum(orth .* repmat(dat(:,j), 1, size(orth, 2)), 1);
+           % Get projection
+           proj = sum( orth .* repmat(components, size(orth, 1), 1), 2);
+           % Get component outside subspace of chosen vectors
+           res(:,j) = dat(:,j) - proj;
+       end
    end
+   
 end
